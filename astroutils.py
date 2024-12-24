9dd4fe7b3d490c8d9ea4b81496caffcbe929ddda
Jose A Dianes <jadianes@gmail.com>
2024-12-24 18:32:20 +0000

    from radians

--- astroutils.py
+++ astroutils.py
@@ -9,7 +9,7 @@ def to_radians(
         degs: typing.Optional[float] = 0.0, 
         arcmins: typing.Optional[float] = 0.0, 
         arcsecs: typing.Optional[float] = 0.0,
-        ) -> float:
+        ) -> str:
     '''Convert degrees, arcminutes, and arcsedons to radians'''
     
     degs_to_radians = degs * (2*math.pi) / 360
@@ -19,3 +19,22 @@ def to_radians(
     res = degs_to_radians + arcmins_to_radians + arcsecs_to_radians
 
     return to_sf(sf=sf, res=res)
+
+
+def from_radians(
+        sf: int,
+        radians: float
+) -> str:
+    '''Convert radians to degrees, arcminutes, and arcseconds'''
+    degrees_f = (radians / (2*math.pi)) * 360
+    print(degrees_f)
+    degrees_f, degrees = math.modf(degrees_f)
+    arcmins_f = degrees_f * 60
+    arcmins_f, arcmins = math.modf(arcmins_f)
+    arcsecs = arcmins_f * 60
+
+    return (
+        to_sf(sf=sf, res=degrees), 
+        to_sf(sf=sf, res=arcmins), 
+        to_sf(sf=sf, res=arcsecs)
+    )
