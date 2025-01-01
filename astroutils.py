922e16e01eed2770714592b3c75768be3a98d092
Jose A Dianes <jadianes@gmail.com>
2025-01-01 20:12:51 +0000

    Finished review of topic 1.2

--- astroutils.py
+++ astroutils.py
@@ -26,7 +26,7 @@ def format_result(
         res: float,
         sf: typing.Optional[int] = None,
         dp: typing.Optional[int] = None,
-) -> str:
+    ) -> str:
     if dp is not None:
         return to_dp(dp=dp, res=res)
     elif sf is not None:
@@ -46,7 +46,7 @@ def to_radians(
         degs: typing.Optional[float] = 0.0, 
         arcmins: typing.Optional[float] = 0.0, 
         arcsecs: typing.Optional[float] = 0.0,
-        ) -> str:
+    ) -> str:
     '''Convert degrees, arcminutes, and arcsedons to radians'''
     
     degs_to_radians = (degs / DEG_IN_A_CIRC) * RAD_IN_A_CIRC
@@ -61,7 +61,7 @@ def to_radians(
 def from_radians(
         sf: int,
         radians: float
-) -> str:
+    ) -> str:
     '''Convert radians to degrees, arcminutes, and arcseconds'''
     
     degrees_f = ( radians / RAD_IN_A_CIRC ) * DEG_IN_A_CIRC
@@ -82,7 +82,7 @@ def ra_from_time_to_deg(
         h: typing.Optional[float] = 0.0,
         m: typing.Optional[float] = 0.0,
         s: typing.Optional[float] = 0.0
-) -> str:
+    ) -> str:
     '''Convert right ascension coordinates 
     in time to decimal degrees'''
 
@@ -96,11 +96,9 @@ def angular_to_linear(
         sf: int,
         theta: float,
         distance_away: float
-) -> str:
+    ) -> str:
     '''Convert angular distance or size to linear'''
-
     res = math.sin(theta) * distance_away
-
     return to_sf(sf=sf, res=res)
 
 
@@ -108,11 +106,9 @@ def magnitude_from_components(
         sf = int, 
         ra_comp = float,
         dec_comp = float
-        ) -> str:
+    ) -> str:
     """Compute magnitude using Pythagoras"""
-    
     res = math.sqrt(ra_comp**2 + dec_comp**2)
-
     return to_sf(sf=sf, res=res)
 
 
@@ -121,10 +117,61 @@ def apparent_to_absolute_magnitude(
         distance_away_in_pc: float,
         sf: typing.Optional[int] = None,
         dp: typing.Optional[int] = None,
-) -> str:
+    ) -> str:
     """Compute absolute magnitude from apparent magnitude via distance"""
-    
     res = apparent_magnitude - 5 * math.log10(distance_away_in_pc) + 5
-
     return format_result(res=res, sf=sf, dp=dp)
 
+
+def magnitude_difference_from_fluxlum_ratio(
+        fluxlum_ratio: float,
+        sf: typing.Optional[int] = None,
+        dp: typing.Optional[int] = None
+    ) -> str:
+    """Calculate magnitude difference from relative fluxlum"""
+    res = 2.5 * math.log10(fluxlum_ratio)
+    return format_result(res=res, sf=sf, dp=dp)
+
+
+def magnitude_from_fluxlum_ratio(
+        magnitude_b: float,
+        b_a_flux_ratio: float,
+        sf: typing.Optional[int] = None,
+        dp: typing.Optional[int] = None
+    ) -> str:
+    """Calculate apparent magnitude from relative 
+    flux and another magnitude
+    """
+    apparent_magnitude_difference = float(
+        apparent_magnitude_difference_from_flux_ratio(
+            flux_ratio=b_a_flux_ratio,
+            sf=sf,
+            dp=dp
+        )
+    )
+    res = apparent_magnitude_difference + magnitude_b
+    return format_result(res=res, sf=sf, dp=dp)
+
+
+def flux_ratio_from_apparent_magnitude_difference(
+        magnitude_a_b_difference: float,
+        sf: typing.Optional[int] = None,
+        dp: typing.Optional[int] = None
+    ) -> str:
+    """"Calculate B/A flux ratio from A/B apparent magnitude difference"""
+    res = 10**(magnitude_a_b_difference/2.5)
+    return format_result(res=res, sf=sf, dp=dp)
+
+
+def flux_ratio_from_apparent_magnitudes(
+        magnitude_a: float,
+        magnitude_b: float,
+        sf: typing.Optional[int] = None,
+        dp: typing.Optional[int] = None
+    ) -> str:
+    """""Calculate B/A flux ratio from two apparent magnitudes"""
+    return flux_ratio_from_apparent_magnitude_difference(
+        magnitude_a_b_difference=magnitude_a-magnitude_b,
+        sf=sf,
+        dp=dp
+    )
